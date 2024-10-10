7669fc89189be0a8319eca8da1896a6f10b28959
Adnan Rashid <58908269+addu2146@users.noreply.github.com>
2024-10-10 03:59:21 -0700

    Create SumOfA.c

---
+++ WEEK5/SumOfA.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <omp.h>
+#include <time.h>
+
+void SumArrayParallel(int arr[], int n, int numt) {
+    double stime, etime, ptime;
+    int Sum = 0;
+    stime = omp_get_wtime();
+    #pragma omp parallel for reduction(+:Sum)
+    for (int i = 0; i < n; i++) {
+        Sum += arr[i];
+    }
+    etime = omp_get_wtime();
+    ptime = (etime - stime) * 1000; // Convert to milliseconds
+    printf("The sum is : %d\n", Sum);
+    printf("Total time taken to execute the loop with %d threads: %f ms\n", numt, ptime);
+}
+
+int main() {
+    double start;
+    double end;
+    double ptime;
+    int n;
+
+    printf("Enter the size of the array\n");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = i + 3;
+    }
+
+    int choice;
+    printf("Press 1 for parallel execution \nPress 2 for serial execution\n");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        int numt;
+        printf("Enter the number of threads you want to use\n");
+        scanf("%d", &numt);
+        omp_set_num_threads(numt);
+        printf("Performing the sum with %d threads\n", numt);
+       SumArrayParallel(arr, n, numt);
+    } else {
+        int Sum = 0;
+        start = clock(); // Start timing here
+        for (int i = 0; i < n; i++) {
+            Sum += arr[i];
+        }
+        end = clock(); // End timing here
+        ptime = (double)(end - start) / CLOCKS_PER_SEC * 1000; // Convert to milliseconds
+        printf("The sum is: %d\n", Sum);
+        printf("The total time taken with serial execution is %f ms\n", ptime);
+    }
+
+    return 0;
+}
